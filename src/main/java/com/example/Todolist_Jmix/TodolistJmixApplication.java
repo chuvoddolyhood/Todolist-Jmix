6683b82a3eba13a81066a527b1d1c753d60c1673
Nghia Tran <trannhannghia.24012000@gmail.com>
2025-12-15 23:02:53 +0700

    init

---
+++ src/main/java/com/example/Todolist_Jmix/TodolistJmixApplication.java
@@ -0,0 +1,13 @@
+package com.example.Todolist_Jmix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistJmixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistJmixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Todolist_Jmix/TodolistJmixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Todolist_Jmix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistJmixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
